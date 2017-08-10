9a80ba43677b894ca51e01d916d66944301df9f0
sedelman25 <sedelman25@gmail.com>
2017-08-10 13:58:05 -0400

    html interactive yayayay

---
+++ startbootstrap-business-casual-4-dev/startbootstrap-business-casual-4-dev/learnHTML.js
@@ -0,0 +1,59 @@
+function showPara() {
+    var tag_list = document.getElementsByClassName('ptag');
+    for (i = 0; i < tag_list.length; i++) {
+        var x = tag_list[i];
+        if (x.style.display === 'block') {
+            x.style.display = 'none';
+        } else {
+            x.style.display = 'block';
+        }
+    }
+}
+
+function showH1() {
+  var tag_list = document.getElementsByClassName('h1tag');
+  for (i = 0; i < tag_list.length; i++) {
+      var x = tag_list[i];
+      if (x.style.display === 'block') {
+          x.style.display = 'none';
+      } else {
+          x.style.display = 'block';
+      }
+  }
+}
+
+function showH2() {
+  var tag_list = document.getElementsByClassName('h2tag');
+  for (i = 0; i < tag_list.length; i++) {
+      var x = tag_list[i];
+      if (x.style.display === 'block') {
+          x.style.display = 'none';
+      } else {
+          x.style.display = 'block';
+      }
+  }
+}
+
+function showH3() {
+  var tag_list = document.getElementsByClassName('h3tag');
+  for (i = 0; i < tag_list.length; i++) {
+      var x = tag_list[i];
+      if (x.style.display === 'block') {
+          x.style.display = 'none';
+      } else {
+          x.style.display = 'block';
+      }
+  }
+}
+
+function showHREF() {
+  var tag_list = document.getElementsByClassName('hreftag');
+  for (i = 0; i < tag_list.length; i++) {
+      var x = tag_list[i];
+      if (x.style.display === 'block') {
+          x.style.display = 'none';
+      } else {
+          x.style.display = 'block';
+      }
+  }
+}
